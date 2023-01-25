446390bbd73bbaeb5c407d7b048dbe33b640fad6
Apple <appleeyespackage@gmail.com>
2023-01-25 04:59:53 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * _printf - this function prints anything
- * @format: format string
- * Return: bytes printed out
+ * _printf - prints anything
+ * @format: the format string
+ *
+ * Return: number of bytes printed
  */
 int _printf(const char *format, ...)
 {
-	int i = 0;
-	va_list list;
+	int sum = 0;
+	va_list ap;
 	char *p, *start;
 	params_t params = PARAMS_INIT;
 
-	va_start(list, format);
+	va_start(ap, format);
 
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
@@ -20,10 +21,10 @@ int _printf(const char *format, ...)
 		return (-1);
 	for (p = (char *)format; *p; p++)
 	{
-		init_params(&params, list);
+		init_params(&params, ap);
 		if (*p != '%')
 		{
-			i += _putchar(*p);
+			sum += _putchar(*p);
 			continue;
 		}
 		start = p;
@@ -32,17 +33,17 @@ int _printf(const char *format, ...)
 		{
 			p++; /* next char */
 		}
-		p = get_width(p, &params, list);
-		p = get_precision(p, &params, list);
+		p = get_width(p, &params, ap);
+		p = get_precision(p, &params, ap);
 		if (get_modifier(p, &params))
 			p++;
 		if (!get_specifier(p))
-			i += print_from_to(start, p,
+			sum += print_from_to(start, p,
 				params.l_modifier || params.h_modifier ? p - 1 : 0);
 		else
-			i += get_print_func(p, ap, &params);
+			sum += get_print_func(p, ap, &params);
 	}
 	_putchar(BUF_FLUSH);
-	va_end(list);
-	return (i);
+	va_end(ap);
+	return (sum);
 }
